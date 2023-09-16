cf3545c6ddae0f8c716a1453799c3dd1390794a7
ibadus <ibadus@icloud.com>
2023-09-17 00:11:08 +0200

    updated main to be functionnal

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "socket.hpp"
 #include "utils.hpp"
 #include "signals.hpp"
+#include "Server.hpp"
 
 #include <iostream>
 #include <string>
@@ -10,7 +11,7 @@
 int main(int argc, char** argv) {
 	unsigned int port;
 	std::string password;
-	
+
 	// validate arguments and set port and password
 	if (!parseArgs(argc, argv, &port, &password)) {
 		std::cerr << TEXT_RED << "Error: Invalid arguments. Use the following format :\n" << argv[0] << " <port> <password>" << TEXT_RESET << std::endl;
@@ -24,10 +25,15 @@ int main(int argc, char** argv) {
 		return 2;
 	}
 	port = getPort(fd);
-	std::cout << TEXT_GREEN << argv[0] << " listening on port " << port << TEXT_RESET << std::endl;
-	
+
 	signal(SIGINT, sigIntHandler); // allows to ctrl+c the server once it's started
-	
-	// TODO: handle the start and all the commands
+
+	try {
+		Server(port, password).start(fd);
+	} catch (std::exception &e) {
+		std::cerr << TEXT_RED << "Unexpected Error: " << e.what() << TEXT_RESET << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
