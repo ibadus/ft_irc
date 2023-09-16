3362e0bfe3f89062dbd30fa95e9de38da5ba3f35
ibadus <ibadus@icloud.com>
2023-09-16 18:32:56 +0200

    addded colors

--- include/server/utils/utils.hpp
+++ include/server/utils/utils.hpp
@@ -3,6 +3,12 @@
 
 #include <string>
 
+#define TEXT_GREEN "\033[0;32m"
+#define TEXT_RED "\033[0;31m"
+#define TEXT_YELLOW "\033[0;33m"
+#define TEXT_BLUE "\033[1;34m"
+#define TEXT_RESET "\033[m"
+
 int toInt(std::string s);
 
 #endif
