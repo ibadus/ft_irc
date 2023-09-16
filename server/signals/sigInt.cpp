34cd044109820e25b755b4305b3549895ed66e3b
ibadus <ibadus@icloud.com>
2023-09-16 21:34:39 +0200

    sigint now set gStatus to STOPPED

--- server/signals/sigInt.cpp
+++ server/signals/sigInt.cpp
@@ -1,8 +1,11 @@
 #include "signals.hpp"
+#include "status.hpp"
+#include "utils.hpp"
 
 #include <iostream>
 
 void sigIntHandler(int s) {
 	(void)s;
-	std::cout << "Interrupt signal received. Exiting..." << std::endl;
+	std::cout << TEXT_YELLOW << "Interrupt signal received." << TEXT_RESET << std::endl;
+	global_status = e_STOPPED;
 }
