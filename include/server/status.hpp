32c20b28d21d1bb828551f347abac3e70506d871
ibadus <ibadus@icloud.com>
2023-09-17 00:10:38 +0200

    renamed enum

--- include/server/status.hpp
+++ include/server/status.hpp
@@ -2,18 +2,18 @@
 #define STATUS_HPP
 
 enum te_status {
-	e_STOPPED = 0,
-	e_RUNNING = 1,
-	e_RESTARTING = 2
+	e_STOP = 0,
+	e_RUN = 1,
+	e_RESTART = 2
 };
 
 /*
-* 0 = stopped, 1 = running, 2 = restarting
+* 0 = stop, 1 = run, 2 = restart
 *
-* set to 0 in server() and 1 in server.start() method
+* initalized to 0 in Server.cpp and 1 in server.start() method
 *
 * Note: state of the server needs to be global to be accessed by the signal handler
 */
-te_status global_status = e_STOPPED;
+extern te_status global_status;
 
 #endif
