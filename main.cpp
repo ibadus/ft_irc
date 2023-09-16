f8f360abcf21c8d9552330d5698db323203e0b18
ibadus <ibadus@icloud.com>
2023-09-16 16:20:29 +0200

    changed error message to cerr

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
 	}
 
 	if (argc != 3) {
-		std::cout << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
 		return 1;
 	}
 
